b9f07876550eb8f5edab50a52054e0dc4bb0697c
amal <amalananana@gmail.com>
2026-01-07 13:00:53 +0200

    newsApiService

---
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,9 @@
+import 'package:retrofit/http.dart';
+
+import '../../../../../core/constants/constants.dart';
+
+part 'News_api_service.g.dart';
+
+@RestApi(baseUrl: newsAPIBaseURL)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
---
+++ lib/features/daily_news/data/repository/article_repository_implementation.dart
@@ -0,0 +1,14 @@
+import 'package:clean_architecture_project/core/resources/data_state.dart';
+import 'package:clean_architecture_project/features/daily_news/data/models/article.dart';
+import 'package:clean_architecture_project/features/daily_news/domain/entities/article.dart';
+import 'package:clean_architecture_project/features/daily_news/domain/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticles() {
+    // TODO: implement getNewsArticles
+    throw UnimplementedError();
+  }
+
+
+}
