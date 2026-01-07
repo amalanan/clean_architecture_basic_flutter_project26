3979484926a43ae6f6d308ac7ebe137e0a19057f
amal <amalananana@gmail.com>
2026-01-07 13:51:15 +0200

    first commit

--- lib/features/daily_news/data/data_sources/remote/news_api_service.dart
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -1,4 +1,3 @@
-
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 
@@ -11,13 +10,10 @@ part 'News_api_service.g.dart';
 abstract class NewsApiService {
   factory NewsApiService(Dio dio) = _NewsApiService;
 
-
   @GET('/top-headlines')
   Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
-@Query("apiKey") String ? apiKey,
-    @Query("country") String ? country,
-    @Query("category") String ? category,
+    @Query("apiKey") String? apiKey,
+    @Query("country") String? country,
+    @Query("category") String? category,
   });
 }
-
-
