313addac827c21e5009f323650addbb355dab3ca
amal <amalananana@gmail.com>
2026-01-11 10:50:04 +0200

    format files

--- lib/features/daily_news/data/data_sources/local/app_database.dart
+++ lib/features/daily_news/data/data_sources/local/app_database.dart
@@ -4,9 +4,10 @@ import 'package:clean_architecture_project/features/daily_news/data/data_sources
 import 'package:clean_architecture_project/features/daily_news/data/models/article.dart';
 import 'package:floor/floor.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
+
 part 'app_database.g.dart';
 
 @Database(version: 1, entities: [ArticleModel])
-abstract class AppDatabase extends FloorDatabase{
-ArticleDao get articleDAO;
+abstract class AppDatabase extends FloorDatabase {
+  ArticleDao get articleDAO;
 }
--- lib/features/daily_news/data/data_sources/local/app_database.g.dart
+++ lib/features/daily_news/data/data_sources/local/app_database.g.dart
@@ -74,11 +74,10 @@ class _$AppDatabase extends AppDatabase {
 
   ArticleDao? _articleDAOInstance;
 
-  Future<sqflite.Database> open(
-    String path,
-    List<Migration> migrations, [
-    Callback? callback,
-  ]) async {
+  Future<sqflite.Database> open(String path,
+      List<Migration> migrations, [
+        Callback? callback,
+      ]) async {
     final databaseOptions = sqflite.OpenDatabaseOptions(
       version: 1,
       onConfigure: (database) async {
@@ -111,37 +110,38 @@ class _$AppDatabase extends AppDatabase {
 }
 
 class _$ArticleDao extends ArticleDao {
-  _$ArticleDao(
-    this.database,
-    this.changeListener,
-  )   : _queryAdapter = QueryAdapter(database),
+  _$ArticleDao(this.database,
+      this.changeListener,)
+      : _queryAdapter = QueryAdapter(database),
         _articleModelInsertionAdapter = InsertionAdapter(
             database,
             'article',
-            (ArticleModel item) => <String, Object?>{
-                  'id': item.id,
-                  'author': item.author,
-                  'title': item.title,
-                  'description': item.description,
-                  'url': item.url,
-                  'urlToImage': item.urlToImage,
-                  'publishedAt': item.publishedAt,
-                  'content': item.content
-                }),
+                (ArticleModel item) =>
+            <String, Object?>{
+              'id': item.id,
+              'author': item.author,
+              'title': item.title,
+              'description': item.description,
+              'url': item.url,
+              'urlToImage': item.urlToImage,
+              'publishedAt': item.publishedAt,
+              'content': item.content
+            }),
         _articleModelDeletionAdapter = DeletionAdapter(
             database,
             'article',
             ['id'],
-            (ArticleModel item) => <String, Object?>{
-                  'id': item.id,
-                  'author': item.author,
-                  'title': item.title,
-                  'description': item.description,
-                  'url': item.url,
-                  'urlToImage': item.urlToImage,
-                  'publishedAt': item.publishedAt,
-                  'content': item.content
-                });
+                (ArticleModel item) =>
+            <String, Object?>{
+              'id': item.id,
+              'author': item.author,
+              'title': item.title,
+              'description': item.description,
+              'url': item.url,
+              'urlToImage': item.urlToImage,
+              'publishedAt': item.publishedAt,
+              'content': item.content
+            });
 
   final sqflite.DatabaseExecutor database;
 
@@ -156,15 +156,16 @@ class _$ArticleDao extends ArticleDao {
   @override
   Future<List<ArticleModel>> getArticles() async {
     return _queryAdapter.queryList('SELECT * FROM article',
-        mapper: (Map<String, Object?> row) => ArticleModel(
-            id: row['id'] as int?,
-            author: row['author'] as String?,
-            title: row['title'] as String?,
-            description: row['description'] as String?,
-            url: row['url'] as String?,
-            urlToImage: row['urlToImage'] as String?,
-            publishedAt: row['publishedAt'] as String?,
-            content: row['content'] as String?));
+        mapper: (Map<String, Object?> row) =>
+            ArticleModel(
+                id: row['id'] as int?,
+                author: row['author'] as String?,
+                title: row['title'] as String?,
+                description: row['description'] as String?,
+                url: row['url'] as String?,
+                urlToImage: row['urlToImage'] as String?,
+                publishedAt: row['publishedAt'] as String?,
+                content: row['content'] as String?));
   }
 
   @override
